8b2ba9bd37567fe8df81d8bb5e309c7cb6ce245c
Jonas Parnow <6gqrbj@posteo.org>
2025-02-09 17:49:16 +0100

    Styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,29 +12,37 @@
 
 <main class="flex w-full max-w-72 flex-col gap-y-5">
 	<header class="flex flex-col text-center">
-		<h1 class="text-xl font-bold">Jonas Parnow</h1>
-		<span>Designer for Information and Data</span>
-		<span class="mt-4 text-xs text-gray-500">Last updated: <time>{updated}</time></span>
+		<h1 class="text-mute decoration-marker text-xl font-bold underline">Jonas Parnow</h1>
+		<span class="text-mute">Designer for Information and Data</span>
+		<span class="text-mute mt-4 text-xs">Last updated: <time>{updated}</time></span>
 	</header>
 	<section>
-		<h2 class="text-xs">Links</h2>
+		<h2 class="text-mute mb-1 text-xs">Links</h2>
 
 		<ul class="flex flex-col gap-y-3">
 			{#each data.links.links_grouped.regular as { label, url }}
 				<li>
-					<a class="block rounded-sm border border-gray-400 p-3" href={url}>
-						<strong class="block text-base/none">{label}</strong>
-						<small class="text-xs/none text-gray-500">{new URL(url).host}</small>
+					<a
+						class="border-border hover:border-blue block rounded-sm border p-3 transition-colors"
+						href={url}
+					>
+						<strong class="text-blue block text-base/none">{label}</strong>
+						<small class="text-mute text-xs/none">{new URL(url).host}</small>
 					</a>
 				</li>
 			{/each}
 		</ul>
 	</section>
 	<section>
-		<h2 class="text-xs">Profiles</h2>
-		<ul class="flex gap-x-2 gap-y-3">
+		<h2 class="text-mute text-xs">Profiles</h2>
+		<ul class="mt-2 flex flex-wrap gap-x-2 gap-y-3">
 			{#each data.links.links_grouped.social as { label, url }}
-				<li><a class="" href={url}>{label}</a></li>
+				<li>
+					<a
+						class="bg-border text-mute hover:bg-blue rounded-md px-3 py-2 text-sm hover:text-white"
+						href={url}>{label}</a
+					>
+				</li>
 			{/each}
 		</ul>
 	</section>
