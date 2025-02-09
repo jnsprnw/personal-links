27f78ed88e55da8d270caa72b53d4b470a90378d
Jonas Parnow <6gqrbj@posteo.org>
2025-02-09 17:55:43 +0100

    Header, Favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	const SITE_TITLE = 'Jonas Parnow: Links';
+	const SITE_URL = 'https://links.jonasparnow.com';
+	const SITE_DESCRIPTION = 'Links to my projects, profiles, and more.';
 </script>
+
+<svelte:head>
+	<title>{SITE_TITLE}</title>
+	<meta name="description" content={SITE_DESCRIPTION} />
+
+	<meta name="robots" content="index, follow, noimageindex" />
+
+	<meta property="og:url" content={SITE_URL} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={SITE_TITLE} />
+	<meta property="og:description" content={SITE_DESCRIPTION} />
+	<meta property="og:site_name" content={SITE_TITLE} />
+	<meta property="og:locale" content="en_GB" />
+	<meta property="article:author" content="Jonas Parnow" />
+	<meta property="og:updated_time" content={new Date().toISOString()} />
+
+	<meta itemprop="name" content={SITE_TITLE} />
+	<meta itemprop="description" content={SITE_DESCRIPTION} />
+
+	<link rel="canonical" href={SITE_URL} />
+</svelte:head>
 
 <div class="flex h-screen w-screen items-center justify-center">
 	{@render children()}
