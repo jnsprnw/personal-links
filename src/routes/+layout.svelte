f75c684e165034af27b9be27a818b30f283e5c9f
Jonas Parnow <6gqrbj@posteo.org>
2025-04-21 17:08:26 +0200

    Packages updated, formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
+	const { children } = $props();
 
 	const SITE_TITLE = 'Jonas Parnow: Links';
 	const SITE_URL = 'https://links.jonasparnow.com';
